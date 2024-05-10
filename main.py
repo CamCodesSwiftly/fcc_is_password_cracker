44d0476acc13efe3fd820d3a225bdc8aa5a032cb
CamCodes Baba <kbabazadeh@icloud.com>
2024-05-10 16:02:15 +0000

    Solve Task 1
    If the password is in the password list,
    return the password.
    Otherwise return "not in db".

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 import password_cracker
 from unittest import main
 
-cracked_password1 = password_cracker.crack_sha1_hash(
-    "fbbe7e952d1050bfb09dfdb71d4c2ff2b3d845d2")
-print(cracked_password1)
+# cracked_password1 = password_cracker.crack_sha1_hash(
+#     "b305921a3723cd5d70a375cd21a61e60aabb84ec")
+# print(cracked_password1)
 
 cracked_password2 = password_cracker.crack_sha1_hash(
-    "dcc466796201f7232b22a03781110a8871fd038c", True)
+    "53d8b3dc9d39f0184144674e310185e41a87ffd5", True)
 print(cracked_password2)
 
 # Run unit tests automatically
-main(module = "test_module", exit = False)
+# main(module = "test_module", exit = False)
--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,60 @@
 import hashlib
 
+def read_passwords():
+    with open("top-10000-passwords.txt", "r") as file:
+        passwords = [line.strip() for line in file.readlines()]
+    return passwords
+
+def read_salts():
+    with open("known-salts.txt", "r") as file:
+        salts = [line.strip() for line in file.readlines()]
+    return salts
+
+def hash_passwords(passwords):
+    hashed_passwords = []
+    for password in passwords:
+        password_bytes = password.encode("utf-8")
+        sha1_hash = hashlib.sha1()
+        sha1_hash.update(password_bytes)
+        hashed_password = sha1_hash.hexdigest()
+        hashed_passwords.append(hashed_password)
+    return hashed_passwords
+
 def crack_sha1_hash(hash, use_salts = False):
-    return True
+    # read 10k passwords
+    passwords = read_passwords()
+
+    # hash 10k passwords
+    hashed_passwords = hash_passwords(passwords)
+
+    # check if the password is in the list
+    if use_salts:
+        salts = read_salts()
+        salted_passwords = hash_passwords_with_salts(passwords, salts)
+
+        #TODO: did the appending/prepending go wrong?
+        print(hash)
+        print(passwords[salted_passwords.index(hash)])
+        if hash in salted_passwords:
+            return passwords[salted_passwords.index(hash)]
+        #TODO: did the appending/prepending go wrong?
+
+    else:
+        if hash in hashed_passwords:
+            return passwords[hashed_passwords.index(hash)]
+        else:
+            return "PASSWORD NOT IN DATABASE"
+
+
+def hash_password_with_salt(password, salt):
+    combined = salt + password + salt
+    hashed = hashlib.sha1(combined.encode()).hexdigest()
+    return hashed
+
+def hash_passwords_with_salts(passwords, salts):
+    hashed_passwords = []
+    for password in passwords:
+        for salt in salts:
+            hashed = hash_password_with_salt(password, salt)
+            hashed_passwords.append(hashed)
+    return hashed_passwords
